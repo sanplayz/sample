fe1ca763d3787acb94e36df03aa7a112707fb62d
connectwithdhanush <dhanushmech1996@gmail.com>
2021-04-14 18:50:02 +0530

    Dhana modified

--- src/main/java/org/sample/Sample1.java
+++ src/main/java/org/sample/Sample1.java
@@ -12,4 +12,16 @@ public class Sample1 {
 	public void method3() {
 		System.out.println("method 3");
 	}
+
+	public void method7() {
+		System.out.println("method7");
+	}
+
+	public void method8() {
+		System.out.println("method8");
+	}
+
+	public void method9() {
+		System.out.println("method9");
+	}
 }
