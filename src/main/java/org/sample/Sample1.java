1918e942d3e00928c88fe59a4e9463d0e2cc26ae
sanplayz <rrsanthosh1997@gmail.com>
2021-04-14 18:29:05 +0530

    master copy

---
+++ src/main/java/org/sample/Sample1.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Sample1 {
+	public void method1() {
+		System.out.println("method 1");
+	}
+
+	public void method2() {
+		System.out.println("method 2");
+	}
+
+	public void method3() {
+		System.out.println("method 3");
+	}
+}
